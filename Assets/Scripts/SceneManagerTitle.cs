712991b2437099bdb19310cea401297514c67e48
hatena75 <aroriva1@yahoo.co.jp>
2019-10-09 12:15:35 +0900

    titleの装飾、フェードアウト実装

--- Assets/Scripts/SceneManagerTitle.cs
+++ Assets/Scripts/SceneManagerTitle.cs
@@ -2,20 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneManagerTitle : MonoBehaviour
 {
+    public GameObject Panel;
+    float a;
+
+    void Awake()
+    {
+        a = Panel.GetComponent<Image>().color.a;
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    IEnumerator FadeOutPanel()
+    {
+        while(a < 1.0f)
+        {
+            Debug.Log(a);
+            Panel.GetComponent<Image>().color += new Color(0, 0, 0, 0.01f);
+            a += 0.01f;
+            yield return null;
+        }
+
+        SceneManager.LoadScene ("Card");
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown (0)) {
-			SceneManager.LoadScene ("Card");
+            StartCoroutine(FadeOutPanel());
 		}
     }
 }
