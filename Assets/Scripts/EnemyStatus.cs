cd5aeaf24fbf111a437eae10951e1ad63fc7d43b
hatena75 <aroriva1@yahoo.co.jp>
2019-11-19 20:20:14 +0900

    HPゲージを実装

--- Assets/Scripts/EnemyStatus.cs
+++ Assets/Scripts/EnemyStatus.cs
@@ -8,12 +8,16 @@ public class EnemyStatus : MonoBehaviour
     public readonly int maxHP = 200;
     private int HP;
     private Text textHP;
+    private Slider barHP;
+
 
     // Start is called before the first frame update
     void Start()
     {
         HP = maxHP;
         textHP = GameObject.Find ("EnemyHP").GetComponent<Text>();
+        barHP = GameObject.Find("EnemyBar").GetComponent<Slider>();
+
     }
 
     public void Damage(int damage)
@@ -24,7 +28,8 @@ public class EnemyStatus : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        textHP.text = "HP:" + HP;
+        textHP.text = HP.ToString();
+        barHP.value = HP;
 
         if(HP <= 0)
         {
--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -9,12 +9,14 @@ public class PlayerStatus : MonoBehaviour
     public readonly int maxHP = 200;
     private int HP;
     private Text textHP;
+    private Slider barHP;
 
     // Start is called before the first frame update
     void Start()
     {
         HP = maxHP;
         textHP = GameObject.Find ("PlayerHP").GetComponent<Text>();
+        barHP = GameObject.Find("PlayerBar").GetComponent<Slider>();
     }
 
     public void Damage(int damage)
@@ -25,7 +27,8 @@ public class PlayerStatus : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        textHP.text = "HP:" + HP;
+        textHP.text = HP.ToString();
+        barHP.value = HP;
 
         if(HP <= 0)
         {
