3eaf762ddd01d8504eb5472a3c50ba4d524f2346
hatena75 <aroriva1@yahoo.co.jp>
2019-12-03 01:15:11 +0900

    表情を変えられる、顔の画像を簡単に変えられるようにした

---
+++ Assets/Scripts/Face.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Faces : int
+{
+    Normal, Attack, Win, Lose
+}
+
+public class Face : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    public Sprite[] faces;
+    [SerializeField] string charName; //Inspectorから書き換え可能
+
+    public void ChangeFace(Faces face){
+        spriteRenderer.sprite = faces[(int)face];
+    }
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        faces = Resources.LoadAll<Sprite> ("pictures/" + charName);
+    }
+
+    void Start()
+    {
+        ChangeFace(Faces.Normal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
