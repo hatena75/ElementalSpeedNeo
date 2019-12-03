4713763947c2cc5619d4ceadc66d68e7aa02a406
hatena75 <aroriva1@yahoo.co.jp>
2019-12-03 13:55:03 +0900

    CommonStatusをCPUに継承させた

--- Assets/Scripts/EnemyStatus.cs
+++ Assets/Scripts/EnemyStatus.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; //added
 
-public class EnemyStatus : MonoBehaviour
+public class EnemyStatus : CommonPlayerStatus
 {
-    public readonly int maxHP = 200;
+    
+    //public readonly int maxHP = 200;
+    /*
     private int HP;
     private int damageSum;
     private Text textHP;
@@ -14,23 +16,26 @@ public class EnemyStatus : MonoBehaviour
     private GameObject face;
 
     private GameObject effect;
+    */
 
 
     // Start is called before the first frame update
     void Start()
     {
-        HP = maxHP;
-        damageSum = 0;
+        base.Start();
+        //HP = maxHP;
+        //damageSum = 0;
         textHP = GameObject.Find ("EnemyHP").GetComponent<Text>();
         barHP = GameObject.Find("EnemyBar").GetComponent<Slider>();
-        damageCount = GameObject.Find ("DamageCount").GetComponent<Text>();
+        //damageCount = GameObject.Find ("DamageCount").GetComponent<Text>();
         face = GameObject.Find ("EnemyCharactor");
 
         //プレハブをGameObject型で取得
-        effect = (GameObject)Resources.Load ("Prefabs/Hit_Fire");
+        //effect = (GameObject)Resources.Load ("Prefabs/Hit_Fire");
         
     }
 
+    /*
     public void Damage(int damage)
     {
         if(damage != 0){
@@ -69,12 +74,14 @@ public class EnemyStatus : MonoBehaviour
         damageSum = 0;
         damageCount.text = damageSum.ToString();//ここで表示されるのは相手からのダメージ
     }
+    */
 
     // Update is called once per frame
     void Update()
     {
-        textHP.text = HP.ToString();
-        barHP.value = HP;
+        base.Update();
+        //textHP.text = HP.ToString();
+        //barHP.value = HP;
         
 
         if(HP <= 0)
