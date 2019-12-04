b78fb3ca5fc9723c9f9e346f926232f8a96c9c21
hatena75 <aroriva1@yahoo.co.jp>
2019-12-04 23:35:05 +0900

    SEを流すための設計、オーディオファイルの追加

---
+++ Assets/Scripts/SEManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEManager : MonoBehaviour {
+    public AudioClip takeCard;
+    public AudioClip playCard;
+    private AudioSource[] audioSource;
+
+    private enum SE : int{
+        Players, Effect
+    }
+
+    void Start () {
+        audioSource = gameObject.GetComponents<AudioSource>();
+        audioSource[(int)SE.Players].clip = null;
+        audioSource[(int)SE.Effect].clip = null;
+    }
+
+    private void PlaySE(SE se, AudioClip audio){
+        audioSource[(int)se].clip = audio;
+        audioSource[(int)se].Play();
+    }
+
+    public void CardTakeSE(){
+        PlaySE(SE.Players, takeCard);
+    }
+
+    public void CardPlaySE(){
+        PlaySE(SE.Players, playCard);
+    }
+
+    void Update () {
+
+    }
+
+}
