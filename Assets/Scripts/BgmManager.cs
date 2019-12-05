7bd8e7fb68f3f9b3d00c8d438c5b76a792437449
hatena75 <aroriva1@yahoo.co.jp>
2019-12-05 16:29:39 +0900

    BGMを追加。AudioListenerをMasterに移動

---
+++ Assets/Scripts/BgmManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgmManager : MonoBehaviour
+{
+    public AudioClip bgm;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = bgm;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
