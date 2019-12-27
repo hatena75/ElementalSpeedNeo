77c7ae988ad213b9ea335b1a10be248a1654f102
hatena75 <aroriva1@yahoo.co.jp>
2019-12-28 02:54:31 +0900

    ボタンの有効、無効を視覚化、外部から切り替え可能に

--- Assets/Scripts/CardScene/HandResetButton.cs
+++ Assets/Scripts/CardScene/HandResetButton.cs
@@ -5,31 +5,40 @@ using UnityEngine.UI;
 
 public class HandResetButton : MonoBehaviour
 {
-    public float timeOut;
-    private float timeElapsed;
+    private bool usable;
+    private Button btn;
+
+    public void Activate(){
+        usable = true;
+        btn.interactable = usable;
+    }
+
+    public void DeActivate(){
+        usable = false;
+        btn.interactable = usable;
+    }
 
     public void OnClick() {
-        if(timeElapsed >= timeOut){
-            
+        if(usable){  
             GameObject[] playerHands = GameObject.FindGameObjectsWithTag("Player");
             foreach (GameObject playerHand in playerHands) {
                 playerHand.GetComponent<CardModel>().RandomFace();
             }
 
-            timeElapsed = 0.0f;
+            DeActivate();
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        timeOut = 3.0f;
-        timeElapsed = 3.0f;
+        btn = gameObject.GetComponent<Button>();
+        DeActivate();
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeElapsed += Time.deltaTime;
+
     }
 }
