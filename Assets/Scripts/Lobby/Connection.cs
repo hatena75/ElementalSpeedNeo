fe9a1e51dcbd703058c07784e75d5004a6bdc64e
hatena75 <aroriva1@yahoo.co.jp>
2020-01-10 00:18:30 +0900

    対人戦において相手を待つロビーと部屋のシーンLobbyを追加

---
+++ Assets/Scripts/Lobby/Connection.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+// MonoBehaviourではなくMonoBehaviourPunCallbacksを継承して、Photonのコールバックを受け取れるようにする
+public class Connection : MonoBehaviourPunCallbacks
+{
+    private StatusFeedBack sfb;
+
+    private void Connect(string gameVersion)
+    {
+        if (PhotonNetwork.IsConnected == false)
+        {
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    private void JoinLobby()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinLobby();
+        }
+    }
+
+    //ランダムな部屋に入室する
+    public void JoinRandomRoom()
+    {
+        if (PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+
+    private void Start() {
+        // PhotonServerSettingsに設定した内容を使ってマスターサーバーへ接続する
+        Connect("1.0"); //バージョン指定
+    }
+
+    // Photonに接続した時
+    public override void OnConnected()
+    {
+        Debug.Log("OnConnected");
+
+        sfb = GameObject.Find("Status").GetComponent<StatusFeedBack>();
+        sfb.StatusConnected();
+ 
+        // ニックネームを付ける
+        //SetMyNickName("Knohhoso");
+    }
+
+    // Photonから切断された時
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("OnDisconnected");
+    }
+ 
+ 
+    // マスターサーバーに接続した時
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster");
+ 
+        // ロビーに入る
+        JoinLobby();
+    }
+ 
+ 
+    // ロビーに入った時
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("OnJoinedLobby");
+
+        JoinRandomRoom();
+    }
+ 
+ 
+    // ロビーから出た時
+    public override void OnLeftLobby()
+    {
+        Debug.Log("OnLeftLobby");
+    }
+ 
+ 
+    // 部屋を作成した時
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("OnCreatedRoom");
+    }
+ 
+ 
+    // 部屋の作成に失敗した時
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnCreateRoomFailed");
+    }
+ 
+ 
+    // 部屋に入室した時
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom");
+
+        sfb.StatusWaiting();
+
+        if(PhotonNetwork.CurrentRoom.MaxPlayers == PhotonNetwork.CurrentRoom.PlayerCount)
+        {
+            Debug.Log("GameStart!");
+            sfb.StatusMatch();
+        }
+        
+    }
+ 
+    // 部屋から退室した時
+    public override void OnLeftRoom()
+    {
+        Debug.Log("OnLeftRoom");
+    }
+ 
+ 
+    // 他のプレイヤーが入室してきた時
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("OnPlayerEnteredRoom");
+
+        if(PhotonNetwork.CurrentRoom.MaxPlayers == PhotonNetwork.CurrentRoom.PlayerCount)
+        {
+            Debug.Log("GameStart!");
+            sfb.StatusMatch();
+        }
+    }
+ 
+ 
+    // 他のプレイヤーが退室した時
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.Log("OnPlayerLeftRoom");
+    }
+
+    // ランダムな部屋への入室に失敗した時
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnJoinRandomFailed");
+        //名前適当な2人部屋を新たに作成
+        PhotonNetwork.CreateRoom(null ,new RoomOptions{MaxPlayers = 2} ,null);
+    }
+}
---
+++ Assets/Scripts/Lobby/StatusFeedBack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StatusFeedBack : MonoBehaviour
+{
+    public Text statusText;
+
+    public void StatusConnected(){
+        statusText.text = "Connected";
+    }
+    public void StatusWaiting(){
+        statusText.text = "Waiting...";
+    }
+
+    public void StatusMatch(){
+        statusText.text = "Match!";
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
