5a5ccf6811dec709cc7dba8ebbfc6603828d5bf1
hatena75 <aroriva1@yahoo.co.jp>
2020-08-14 04:09:48 +0900

    キャラクター選択用プログラム

---
+++ Assets/Scripts/CharacterSelectScene/CharacterAbstract.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharactorAbstruct : MonoBehaviour
+{
+    private string name;
+    private string picture;
+
+    protected abstract void Skill();
+
+    public CharactorAbstruct(string name, string picture){
+        this.name = name;
+        this.picture = picture;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CharacterSelectScene/SceneManagerCharacterSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagerCharacterSelect : MonoBehaviour
+{
+    private static string usingCharacter; //自分の使用キャラ
+
+    public static string UsingCharacter { get => usingCharacter; }
+
+    public void SelectCharacter(string character){
+        usingCharacter = character;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
