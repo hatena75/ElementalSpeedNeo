fcf103859ee57f2d1f355986e7b4fa7cc5634f79
hatena75 <aroriva1@yahoo.co.jp>
2020-08-15 01:58:58 +0900

    キャラクター(Enemy)追加

---
+++ Assets/Scripts/CharacterSelectScene/Masato.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Masato : CharacterAbstract
+{
+    public override void Skill(){
+        
+    }
+
+    public Masato() : base(){
+        this.name = "Masato";
+        this.picture = "pictures/Enemy";
+    }
+}
