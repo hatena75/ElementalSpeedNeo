264c396a67d2047a73737fc05d0c00011411e4b4
hatena75 <aroriva1@yahoo.co.jp>
2020-08-16 14:33:19 +0900

    ボタン機能の有効化とシーン遷移の作成

---
+++ Assets/Scripts/CharacterSelectScene/CharacterSelectButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectButton : MonoBehaviour
+{
+     private SceneManagerCharacterSelect sc;
+    
+    public void OnClickYuuki() {
+        sc.SelectCharacter(new Yuuki());
+    }
+
+    public void OnClickMasato() {
+        sc.SelectCharacter(new Masato());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sc = GameObject.Find ("Master").GetComponent<SceneManagerCharacterSelect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/CharacterSelectScene/SceneManagerCharacterSelect.cs
+++ Assets/Scripts/CharacterSelectScene/SceneManagerCharacterSelect.cs
@@ -1,26 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneManagerCharacterSelect : MonoBehaviour
 {
+    public GameObject panel;
+    float a;
+
     private static CharacterAbstract usingCharacter; //自分の使用キャラ
 
     public static CharacterAbstract UsingCharacter { get => usingCharacter; }
 
     public void SelectCharacter(CharacterAbstract character){
         usingCharacter = character;
+        MainLoad();
+    }
+
+    public void MainLoad(){
+        StartCoroutine(FadeOutpanel());
+    }
+
+    void Awake(){
+        a = panel.GetComponent<Image>().color.a;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(FadeInpanel());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator FadeInpanel()
+    {
+        while(a > 0.0f)
+        {
+            //Debug.Log(a);
+            panel.GetComponent<Image>().color -= new Color(0, 0, 0, 0.01f);
+            a -= 0.01f;
+            yield return null;
+        }
+
+        panel.SetActive(false);
+    }
+
+    IEnumerator FadeOutpanel()
+    {
+        panel.SetActive(true);
+
+        while(a < 1.0f)
+        {
+            //Debug.Log(a);
+            panel.GetComponent<Image>().color += new Color(0, 0, 0, 0.01f);
+            a += 0.01f;
+            yield return null;
+        }
+
+        SceneManager.LoadScene ("Card");
     }
 }
