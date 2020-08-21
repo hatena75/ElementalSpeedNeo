b794295759870f6e3fc749dcbe1f9a5f6eba733c
hatena75 <aroriva1@yahoo.co.jp>
2020-08-21 23:50:45 +0900

    Revert "キャラセレのパネル有効化"
    This reverts commit 0a5893e4803426b7884e6e8bb449088b41d6deed.

--- Assets/Scripts/CharacterSelectScene/Haruka.cs
+++ Assets/Scripts/CharacterSelectScene/Haruka.cs
@@ -4,31 +4,7 @@ using UnityEngine;
 
 public class Haruka : CharacterAbstract
 {
-    private void EffectCard(string target){
-        GameObject[] Cards = GameObject.FindGameObjectsWithTag(target);
-        var rand = new System.Random();
-        foreach (GameObject card in Cards) {
-            //属性を除いたカード情報の取得
-            int cardInfo = card.GetComponent<CardModel>().cardIndex % 12;
-            int element = rand.Next(0, 3);
-            int index = cardInfo + (element * 12);
-            card.GetComponent<CardModel>().ChangeFace(index);
-        }
-    }
     public override void Skill(){
-        //相手の全てのカードの攻撃力を10にする。
-        if(PhotonNetwork.IsConnected){
-            if(PhotonNetwork.IsMasterClient){
-                EffectCard("Player2");
-            }
-            else{
-                EffectCard("Player");
-            }
-        }
-        else
-        {
-            EffectCard("Enemy");
-        }
     }
 
     public Haruka() : base(){
