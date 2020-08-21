75d149561c853d1cccd23daba279eaade39c0e31
hatena75 <aroriva1@yahoo.co.jp>
2020-08-21 23:30:42 +0900

    キャラクター「ハルカ」追加

---
+++ Assets/Scripts/CharacterSelectScene/Haruka.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Haruka : CharacterAbstract
+{
+    public override void Skill(){
+    }
+
+    public Haruka() : base(){
+        this.Name = "Haruka";
+        this.Picture = "pictures/Haruka";
+    }
+}
