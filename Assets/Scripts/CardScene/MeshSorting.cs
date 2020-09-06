14813727e53a151bcee94ffe76cd8a9fc78be3e9
hatena75 <aroriva1@yahoo.co.jp>
2020-09-07 00:56:46 +0900

    対戦シーンに演出バナーを最前面にくるよう実装

---
+++ Assets/Scripts/CardScene/MeshSorting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSorting : MonoBehaviour
+{
+    [SerializeField]
+    private string m_layerName = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var render = GetComponent< MeshRenderer >();
+        if( render != null )
+        {
+            render.sortingLayerName = m_layerName;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
