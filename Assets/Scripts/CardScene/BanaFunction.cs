d41237b2c4b867a4c64b603be0a451efe2d8a9a7
hatena75 <aroriva1@yahoo.co.jp>
2020-09-07 22:42:12 +0900

    対戦開始前のバナーのアニメーションタイミングをプログラムから指定できるようにした。

---
+++ Assets/Scripts/CardScene/BanaFunction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BanaFunction : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshPro banaText;
+    private Animator animator;
+    [SerializeField]
+    private GameObject bana;
+
+    public void TextureChangeGo(){
+        banaText.text = "GO!";
+    }
+
+    public void Animation(){
+        //bana.SetActive(true);
+        //animator.Play("ReadyAnimation");
+        animator.enabled = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent (typeof(Animator)) as Animator;
+        animator.enabled = false;
+        //this.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
