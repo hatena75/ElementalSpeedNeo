0727c2dd1a0daad024c4a3cc84ae9ea659442a25
hatena75 <aroriva1@yahoo.co.jp>
2020-09-15 00:20:11 +0900

    対人戦の時、対戦相手の行動によるSEが鳴らないのを修正

--- Assets/Scripts/CardScene/SEManager.cs
+++ Assets/Scripts/CardScene/SEManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Photon.Pun;
 
 public class SEManager : MonoBehaviour {
     public AudioClip takeCard;
@@ -18,43 +20,72 @@ public class SEManager : MonoBehaviour {
         Players, Effect
     }
 
+    private PhotonView photonView;
+
+    private Dictionary<String, AudioClip> AudioChart;
+
     void Start () {
         audioSource = gameObject.GetComponents<AudioSource>();
         audioSource[(int)SE.Players].clip = null;
         audioSource[(int)SE.Effect].clip = null;
+
+        photonView = GetComponent<PhotonView>();
+
+        //属性相性の定義
+        AudioChart = new Dictionary<string, AudioClip>();
+        AudioChart.Add("takeCard", takeCard);
+        AudioChart.Add("playCard", playCard);
+        AudioChart.Add("reload", reload);
+        AudioChart.Add("attack", attack);
+        AudioChart.Add("missingAttack", missingAttack);
+        AudioChart.Add("changeElement", changeElement);
+        AudioChart.Add("down", down);
+
     }
 
-    private void PlaySE(SE se, AudioClip audio){
+    private void PlaySE(SE se, string audio){
+        photonView.RPC("PlaySERpc", RpcTarget.All, (int)se, audio);
+    }
+
+    
+    private void PlaySESingle(SE se, AudioClip audio){
         audioSource[(int)se].clip = audio;
         audioSource[(int)se].Play();
     }
+    
+
+    [PunRPC]
+    private void PlaySERpc(int se, string audio){
+        audioSource[se].clip = AudioChart[audio];
+        audioSource[se].Play();
+    }
 
     public void CardTakeSE(){
-        PlaySE(SE.Players, takeCard);
+        PlaySE(SE.Players, "takeCard");
     }
 
     public void CardPlaySE(){
-        PlaySE(SE.Players, playCard);
+        PlaySE(SE.Players, "playCard");
     }
 
     public void ReloadSE(){
-        PlaySE(SE.Players, reload);
+        PlaySE(SE.Players, "reload");
     }
 
     public void AttackSE(){
-        PlaySE(SE.Effect, attack);
+        PlaySESingle(SE.Effect, attack);
     }
 
     public void MissingAttackSE(){
-        PlaySE(SE.Players, missingAttack);
+        PlaySESingle(SE.Players, missingAttack);
     }
 
     public void ChangeElementSE(){
-        PlaySE(SE.Players, changeElement);
+        PlaySE(SE.Players, "changeElement");
     }
 
     public void DownSE(){
-        PlaySE(SE.Players, down);
+        PlaySE(SE.Players, "down");
     }
 
     void Update () {
