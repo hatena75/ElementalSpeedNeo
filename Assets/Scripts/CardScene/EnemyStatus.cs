124e537f0ef958def9fc5f4d1d84c1415d078b9e
hatena75 <hatena75@outlook.jp>
2021-05-08 19:17:33 +0900

    プレイヤー2のHPバー表示が逆になっていたバグを修正

--- Assets/Scripts/CardScene/EnemyStatus.cs
+++ Assets/Scripts/CardScene/EnemyStatus.cs
@@ -8,18 +8,10 @@ public class EnemyStatus : CommonPlayerStatus
 {
     void Start()
     {
-        base.Start();
-        if(!PhotonNetwork.IsMasterClient){
-            textHP = GameObject.Find ("PlayerHP").GetComponent<Text>();
-            barHP = GameObject.Find("PlayerBar").GetComponent<Slider>();
-            face = GameObject.Find ("PlayerCharactor");
-        }
-        else
-        {
-            textHP = GameObject.Find ("EnemyHP").GetComponent<Text>();
-            barHP = GameObject.Find("EnemyBar").GetComponent<Slider>();
-            face = GameObject.Find ("EnemyCharactor");
-        }
+        base.Start();        
+        textHP = GameObject.Find ("EnemyHP").GetComponent<Text>();
+        barHP = GameObject.Find("EnemyBar").GetComponent<Slider>();
+        face = GameObject.Find ("EnemyCharactor");
     }
 
     void Update()
--- Assets/Scripts/CardScene/PlayerStatus.cs
+++ Assets/Scripts/CardScene/PlayerStatus.cs
@@ -9,18 +9,9 @@ public class PlayerStatus : CommonPlayerStatus
     void Start()
     {
         base.Start();
-
-        if(PhotonNetwork.IsMasterClient){
-            textHP = GameObject.Find ("PlayerHP").GetComponent<Text>();
-            barHP = GameObject.Find("PlayerBar").GetComponent<Slider>();
-            face = GameObject.Find ("PlayerCharactor");
-        }
-        else
-        {
-            textHP = GameObject.Find ("EnemyHP").GetComponent<Text>();
-            barHP = GameObject.Find("EnemyBar").GetComponent<Slider>();
-            face = GameObject.Find ("EnemyCharactor");
-        }
+        textHP = GameObject.Find ("PlayerHP").GetComponent<Text>();
+        barHP = GameObject.Find("PlayerBar").GetComponent<Slider>();
+        face = GameObject.Find ("PlayerCharactor");
     }
 
     void Update()
